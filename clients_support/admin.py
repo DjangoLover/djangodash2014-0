5baf0d0636e013c6209d8e7316567fc89af8c75f
lionasp <lion.programmer@gmail.com>
2013-09-28 20:21:43 +0300

    update ticket admin

--- clients_support/admin.py
+++ clients_support/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 from clients_support.models import Ticket, TicketType, Tag
 
 
-admin.site.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'user', 'manager', 'status', 'type', 'importance', 'updated_time')
+    list_filter = ('tags', 'type', 'importance', 'status', 'created_time')
+    search_fields = ('subject', 'text')
+
+
+admin.site.register(Ticket, TicketAdmin)
 admin.site.register(TicketType)
