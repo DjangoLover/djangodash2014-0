5cba9a0ebd735d57fccb0c79971110693a882505
lionasp <lion.programmer@gmail.com>
2013-09-29 01:13:27 +0300

    add static block for tickets

--- clients_support/templatetags/block_with_statistics.py
+++ clients_support/templatetags/block_with_statistics.py
@@ -1,7 +1,22 @@
+from datetime import datetime, time, timedelta
 from django import template
+
+from clients_support.models import Ticket
 
 register = template.Library()
 
 @register.inclusion_tag('clients_support/block_with_statistics.html')
 def block_with_statistics():
-    return {}
+    today = datetime.now().date()
+    tomorrow = today + timedelta(1)
+
+    today = datetime.combine(today, time())
+    tomorrow = datetime.combine(tomorrow, time())
+    
+    return {
+        'closed_count': Ticket.objects.filter(status=Ticket.CLOSED_STATUS).count(),
+        'open_count': Ticket.objects.exclude(status=Ticket.CLOSED_STATUS).exclude(status=Ticket.SOLVED_STATUS).count(),
+        'total_count': Ticket.objects.all().count(),
+        'add_today_count': Ticket.objects.filter(created_time__gte=today).filter(created_time__lte=tomorrow).count(),
+        'closed_today_count': Ticket.objects.filter(closed_time__gte=today).filter(closed_time__lte=tomorrow).count()
+    }
